84bb14587adcef19c3a708b59730262f8dbc658d
Luo Shang <1363477852@qq.com>
2024-03-03 21:27:27 +0800

    init

---
+++ ej-back/app.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2022, Shang Luo
+# All rights reserved.
+# 
+# Author: 罗尚
+# Building Time: 2024/3/3
+# Reference: None
+# Description: None
+
+
+if __name__ == '__main__':
+    ...
