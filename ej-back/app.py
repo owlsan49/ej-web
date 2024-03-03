0c0214c92c64518835ffa21d327848745e80ccc4
Luo Shang <1363477852@qq.com>
2024-03-03 21:49:57 +0800

    init

--- ej-back/app.py
+++ ej-back/app.py
@@ -6,7 +6,20 @@
 # Building Time: 2024/3/3
 # Reference: None
 # Description: None
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from utils import read_json
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/plus_one', methods=['GET'])
+def push_words():
+    results = {'resCode': 0}
+    return jsonify(results)
 
 
 if __name__ == '__main__':
-    ...
+    port = read_json('../config.json')['port']
+    app.run(debug=True, port=port)
---
+++ ej-back/utils.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2022, Shang Luo
+# All rights reserved.
+# 
+# Author: 罗尚
+# Building Time: 2024/3/3
+# Reference: None
+# Description: None
+import json
+import re
+import copy
+import random
+
+from datetime import datetime
+
+
+def read_json(data_path):
+    try:
+        with open(data_path, 'r', encoding='utf-8') as jf:
+            data = json.load(jf)
+    except Exception as e:
+        print(e)
+        print(f'{data_path} is Null')
+        data = {}
+    return data
+
+
+def write_json(file_name, json_dict, mode='w'):
+    with open(file_name, mode, encoding='utf-8') as jf:
+        json.dump(json_dict, jf)
+
+
+if __name__ == '__main__':
+    ...
